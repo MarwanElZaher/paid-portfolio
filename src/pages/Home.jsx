e9b5bd2f38206e07b4a39335e831c4c6b5f6155f
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-10-04 20:44:32 +0300

    feat: home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,7 +30,7 @@ const Home = () => {
             
               <h1
                 ref={videoRef}
-                className="uppercase cursor-default font-bold text-[11rem] hover:font-light duration-200 "
+                className="uppercase cursor-default font-clash font-clash font-bold text-[11rem] hover:font-light duration-200 "
               >
                 Hossam Saieed
                 </h1>
@@ -43,11 +43,11 @@ const Home = () => {
       <section className="flex flex-col justify-center items-center min-h-screen">
       <div className={`flex flex-col items-center text-center gap-3 max-w-6xl transition-all duration-1000 ${textVisible ? 'scale-95' : 'scale-75'}`}>
       <img src={DiamondIcon} alt='diamondIcon' />
-          <h1 ref={textRef} className='uppercase font-medium text-7xl px-10'>{homePageSecondSectionText}</h1>
+          <h1 ref={textRef} className='uppercase font-clash font-medium text-7xl px-10'>{homePageSecondSectionText}</h1>
           <img src={DiamondIcon} alt='diamondIcon' />
           <h2 className='font-normal text-3xl mx-52'>{homePageSecondSectionSecondText }</h2>
           <CustomButton
-            className='uppercase rounded-full bg-black text-white p-4 font-medium hover:bg-[#6e6e6b] hover:scale-110 transition-all duration-300'
+            className='uppercase rounded-full bg-black text-white p-4 font-clash font-medium hover:bg-[#6e6e6b] hover:scale-110 transition-all duration-300'
             text="Learn More"
             to="/about"
           />
@@ -56,13 +56,13 @@ const Home = () => {
       <section className="flex flex-col justify-center items-center min-h-screen">
         <div className={`gap-[56px] border-t border-b border-grey-800 flex flex-col items-center pt-[80px] pb-[80px] transition-all duration-1000 ${recentProjectsVisible ? 'scale-90' : 'scale-75'}`}>
            <div className='flex flex-col items-center'>
-             <p  className='uppercase font-medium text-7xl px-10 m-1'>{recentProjectsTitleFirstPart}</p>
-              <p className='uppercase font-medium text-7xl px-10 m-1'>{recentProjectsTitleSecondPart}</p>
+             <p  className='uppercase font-clash font-medium text-7xl px-10 m-1'>{recentProjectsTitleFirstPart}</p>
+              <p className='uppercase font-clash font-medium text-7xl px-10 m-1'>{recentProjectsTitleSecondPart}</p>
           </div>
           <div ref={projectsRef} className={`flex flex-col justify-center items-center gap-10 transition-all duration-1000 ${recentProjectsVisible ? 'translate-y-0 opacity-100' : 'translate-y-20 opacity-0'}`}>
           <ProjectList RecentProjects={recentProjects} />
           <CustomButton
-            className='uppercase rounded-full bg-black text-white p-4 font-medium hover:bg-[#6e6e6b] hover:scale-110 transition-all duration-300'
+            className='uppercase rounded-full bg-black text-white p-4 font-clash font-medium hover:bg-[#6e6e6b] hover:scale-110 transition-all duration-300'
             text="View More"
             to="/portfolio"
             />
@@ -71,7 +71,7 @@ const Home = () => {
       
       </section>
       <section className="flex flex-col justify-around items-start my-12 min-h-96">
-        <h1 className='uppercase font-medium text-7xl px-10 m-1'>{whatIcanDoTitle}</h1>
+        <h1 className='uppercase font-clash font-medium text-7xl px-10 m-1'>{whatIcanDoTitle}</h1>
         <div className='flex flex-row'>
           {providedServices.map((service, index) => (
             <Card
