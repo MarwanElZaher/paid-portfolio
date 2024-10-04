9b40aef21482491ea2ac6d8bbf757b514e8106ec
Marwan Wael El Zaher <m.wael@penta-b.com>
2024-10-04 20:45:22 +0300

    feat: added all texts inside constants for all pages

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,9 @@ import SecondProjectImg from './assets/second.png'
 import ThirdProjectImg from './assets/third.png'
 import serviceIconCube from './assets/IconWrapper.png'
 import serviceIconSearch from './assets/IconWrapper2.png'
+import firstContactIcon from './assets/contactFirst.svg'
+import secondContactIcon from './assets/contactSecond.svg'
+import thirdContactIcon from './assets/contactThird.svg'
 
 export const homePageSecondSectionText = `I'm a dynamic UI/UX designer / Product Designer
  turning visionary concepts into seamless and impactful digital experiences.`
@@ -14,6 +17,34 @@ export const bannerFooterSecondText = "Reach out and let's bring your ideas to l
 export const whatIcanDoTitle = "What i can do";
 export const aboutTitle = "behind the creativity";
 export const aboutDescription = "I am a passionate and versatile UI/UX designer and product designer, weaving a tapestry of digital experiences that inspire and engage."
+export const secondSectionAbout = "I am a dynamic UI/UX and product designer, passionate about transforming visionary concepts into engaging digital experiences. With a strong foundation in design principles and user-centered methodologies, my mission is to craft solutions that not only meet but exceed expectations. I thrive in collaborative environments where creativity and technical excellence come together seamlessly."
+export const hossamSignature = "Hossam Saieed — UI UX designer / Product Designer"
+export const statisticsTitle = "Tech at a Glance"
+export const statisticsCaption = "Testament to my commitment to excellence, innovation, and impactful solutions."
+export const statistics = [
+  {
+    title: "20+ Projects",
+    description: "Successfully delivered over 20 diverse projects, showcasing remarkable adaptability and expertise across various domains."
+  },
+  {
+    title: "98% Satisfaction",
+    description: "Achieved a remarkable 98% client satisfaction rate, demonstrating a commitment to meeting and exceeding client expectations."
+  },
+  {
+    title: "2+ Innovations",
+    description: "Implemented two innovative solutions, showcasing a proactive approach to problem-solving and staying ahead of industry standards."
+  }
+];
+
+export const valuedClients = "My Valued Clients"
+// services section
+export const serviceTitle = "My tech Services"
+export const serviceDescription = "Innovating Beyond Limits to Craft Exceptional Digital Experiences"
+export const secondSectionTitle = "My services"
+export const thirdSectionTitle = "Strategic Ingenuity"
+export const thirdSectionFirstParagraph = "In the dynamic world of digital innovation, I specialize in harnessing the power of strategic thinking as a UI/UX and product designer. My expertise lies in crafting user-centered solutions that not only align seamlessly with your unique vision but also exceed expectations."
+export const thirdSectionSecondParagraph = "Whether it's the intricacies of intuitive user interfaces, the seamless experiences demanded by mobile applications, or the thoughtful design of compelling products, each project reflects my commitment to strategic design excellence. I don’t just design; I embark on a collaborative journey where every decision is intentional, and every solution is meticulously tailored to elevate user engagement and drive success."
+//home section
 export const recentProjects = [
     {
       imgSrc: FirstProjectImg,
@@ -49,7 +80,7 @@ export const recentProjects = [
       approach: "Utilized Vite for build optimization and lazy loading of assets, deployed on Netlify for continuous integration and delivery."
     }
   ];
-  
+// included in home & services
 export const providedServices = [
   {
     "title": "Product Design",
@@ -62,3 +93,26 @@ export const providedServices = [
     "image": serviceIconSearch
   }
 ];
+
+//contactMe section
+export const contactMeTitle = "Let's Connect and innovate";
+export const contactMeDescription = "Feel free to reach out and commence the journey of collaboration."
+export const ContactMeSecondSectionTitle = "Reach Out & Connect";
+export const contactMeWays = [
+  {
+    "title": "Drop a Message",
+    "description": "For inquiries, collaborations, or just to say hello, feel free to send an email.",
+    "image": firstContactIcon
+  },
+  {
+    "title": "Let's Chat",
+    "description": "Reach out via phone, and let's discuss how we can turn your ideas into reality.",
+    "image": secondContactIcon
+  },
+  {
+    "title": "Schedule a Call",
+    "description": "For a more in-depth conversation or if you have a specific project in mind.",
+    "image": thirdContactIcon
+  }
+];
+export const formTitle = "Let’s bring your vision to life with actionable results";
